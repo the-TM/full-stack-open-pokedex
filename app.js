e997d3a3817e22646f632c19f3ca4936fc23dac7
the-TM <73825132+the-TM@users.noreply.github.com>
2021-06-10 13:12:25 +0300

    11.11 Health check Add healthcheck to deploy-heroku worklow & Add heroku buildpack to access source_version as env.var

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const app = express()
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
-
+const version = process.env.SOURCE_VERSION || 'local'
 app.use(express.static('dist'))
+
+app.get('/version', (req, res) => {
+  res.send(version) // change this string to ensure a new version deployed
+})
 
 app.listen(PORT, () => {
   console.log('server started on port 5000')
--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
-
+console.log(String(process.env.SOURCE_VERSION || 'local'))
 ReactDOM.render(<App />, document.getElementById('app'))
